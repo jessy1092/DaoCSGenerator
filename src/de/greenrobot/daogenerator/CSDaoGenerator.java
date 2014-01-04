49dc39996d6e1a36a1e6a97a477aa2187dd35cbd
Lee <jessy1092@gmail.com>
2014-01-05 01:31:37 +0800

    Generate barcode and history entity.
    Signed-off-by: Lee <jessy1092@gmail.com>

--- src/de/greenrobot/daogenerator/CSDaoGenerator.java
+++ src/de/greenrobot/daogenerator/CSDaoGenerator.java
@@ -29,7 +29,43 @@ public class CSDaoGenerator
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-
+		Schema schema = new Schema(1, "com.openeatsCS.app.model");
+		
+		addTable(schema);
+		try
+		{
+			new DaoGenerator().generateAll(schema, "C:/Users/Lee/OpenEats/OpeneatsCrowdsourcingApp/src-gen");
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.toString());
+		}
+	}
+	
+	private static void addTable(Schema schema)
+	{
+		
+		Entity barcode = schema.addEntity("Barcode");
+		barcode.addIdProperty();
+		barcode.addStringProperty("barcode").unique();
+		barcode.addStringProperty("name").unique();
+		barcode.addStringProperty("loc_photo1");
+		barcode.addStringProperty("loc_photo2");
+		barcode.addStringProperty("loc_photo3");
+		barcode.addBooleanProperty("upload");			// Upload or not
+		barcode.addBooleanProperty("finish");			// Need to upload or not
+		barcode.addBooleanProperty("update");			// update or not
+		
+		Entity history = schema.addEntity("History");
+		history.addIdProperty();
+		history.addDateProperty("created_at");			// Recode scan photo time
+		history.addDateProperty("upload_at");           // Recode upload photo time
+		history.addDateProperty("updated_at");			// Recode updated from server
+		Property barcode_id = history.addLongProperty("barcode_id").notNull().getProperty();
+		
+		// Control Relation manually.
+		barcode.addToMany(history, barcode_id);
+		history.addToOne(barcode, barcode_id);
 	}
 
 }
